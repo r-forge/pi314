5cf10cd862d5250b649bb9dec8c2ee2740bfcc16
hfang (Dr ) <hfang.shanghai@gmail.com>
2017-01-17 11:12:54 +0000

    Update to the version 1.1.4
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pi314@47 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/xPierMatrix.r
+++ pkg/R/xPierMatrix.r
@@ -3,11 +3,17 @@
 #' \code{xPierMatrix} is supposed to extract priority matrix from a list of pNode objects 
 #'
 #' @param list_pNode a list of "pNode" objects
-#' @param displayBy which priority will be extracted. It can be "score" for priority score (by default), "rank" for priority rank
+#' @param displayBy which priority will be extracted. It can be "score" for priority score (by default), "rank" for priority rank, "pvalue" for priority p-value
 #' @param combineBy how to resolve nodes/targets from a list of "pNode" objects. It can be "intersect" for intersecting nodes (by default), "union" for unionising nodes
+#' @param aggregateBy the aggregate method used. It can be either "none" for no aggregation, or "orderStatistic" for the method based on the order statistics of p-values, "fishers" for Fisher's method, "Ztransform" for Z-transform method
 #' @param verbose logical to indicate whether the messages will be displayed in the screen. By default, it sets to true for display
 #' @return
-#' a data frame containing priority matrix.
+#' If aggregateBy is 'none' (by default), a data frame containing priority matrix, with each column for either priority score, or priorty rank or priority p-value.
+#' If aggregateBy is not 'none', an object of the class "aTarget", a list with following components:
+#' \itemize{
+#'  \item{\code{priority}: a data frame of nGene X 5 containing gene priority (aggregated) information, where nGene is the number of genes, and the 5 columns are "name" (gene names), "rank" (ranks of the priority scores), "pvalue" (the aggregated p-value, converted from empirical cumulative distribution of the probability of being GSP), "fdr" (fdr adjusted from the aggregated p-value), "priority" (-log10(fdr))}}
+#'  \item{\code{call}: the call that produced this result}
+#' }
 #' @note none
 #' @export
 #' @seealso \code{\link{xPierSNPs}}
@@ -22,11 +28,12 @@
 #' df_score <- xPierMatrix(ls_pNode)
 #' }
 
-xPierMatrix <- function(list_pNode, displayBy=c("score","rank"), combineBy=c('intersect','union'), verbose=TRUE)
+xPierMatrix <- function(list_pNode, displayBy=c("score","rank","pvalue"), combineBy=c('intersect','union'), aggregateBy=c("none","orderStatistic","fishers","Ztransform"), verbose=TRUE)
 {
 
     displayBy <- match.arg(displayBy)
     combineBy <- match.arg(combineBy)
+    aggregateBy <- match.arg(aggregateBy) 
     
    	if(any(class(list_pNode) %in% c("pNode"))){
 		list_pNode <- list(list_pNode)
@@ -55,8 +62,8 @@ xPierMatrix <- function(list_pNode, displayBy=c("score","rank"), combineBy=c('in
 	ls_priority <- lapply(list_pNode, function(pNode){
 		p <- pNode$priority
 		ind <- match(nodes, rownames(p))
-		ind <- ind[!is.na(ind)]
-		if(displayBy=='score'){
+		#ind <- ind[!is.na(ind)]
+		if(displayBy=='score' | displayBy=='pvalue'){
 			res <- p[ind, c("priority")]
 		}else if(displayBy=='rank'){
 			res <- p[ind, c("rank")]
@@ -64,6 +71,52 @@ xPierMatrix <- function(list_pNode, displayBy=c("score","rank"), combineBy=c('in
 	})
 	df_priority <- do.call(cbind, ls_priority)
 	rownames(df_priority) <- nodes
+	
+	## replace NA with worst value
+	if(displayBy=='score' | displayBy=='pvalue'){
+		df_priority[is.na(df_priority)] <- 0
+	}else if(displayBy=='rank'){
+		df_priority[is.na(df_priority)] <- length(nodes)
+	}
+	
+	## only when displayBy=='pvalue'
+	## Convert into p-values by computing an empirical cumulative distribution function
+	if(displayBy=='pvalue'){
+		df_full <- df_priority
+		ls_pval <- lapply(1:ncol(df_full), function(j){
+			x <- df_full[,j]
+			my.CDF <- stats::ecdf(x)
+			pval <- 1 - my.CDF(x)
+		})
+		df_pval <- do.call(cbind, ls_pval)
+		rownames(df_pval) <- rownames(df_full)
+		colnames(df_pval) <- colnames(df_full)
+		
+		df_priority <- df_pval
+		## aggregate p values
+		if(aggregateBy != "none"){
+			df_ap <- dnet::dPvalAggregate(pmatrix=df_priority, method=aggregateBy)
+			
+			df_ap <- sort(df_ap, decreasing=FALSE)
+			## get rank
+			df_rank <- rank(df_ap, ties.method="min")
+			######
+			df_ap[df_ap==0] <- min(df_ap[df_ap!=0])
+			######
+			## adjp
+			df_adjp <- stats::p.adjust(df_ap, method="BH")
+			
+			df_priority <- data.frame(name=names(df_ap), rank=df_rank, pvalue=df_ap, fdr=df_adjp, priority=-log10(df_adjp), stringsAsFactors=FALSE)
+			
+			aTarget <- list(priority = df_priority,
+							Call     = match.call()
+						 )
+			class(aTarget) <- "aTarget"
+			
+			df_priority <- aTarget
+		}
+		
+	}
 	
     invisible(df_priority)
 }
